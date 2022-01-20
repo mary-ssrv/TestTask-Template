5468abc7cc4980643ed9e6c36c3a6cb7ba3db8c0
Maria Sergeeva <saylinboice@gmail.com>
2022-01-20 09:04:20 +0300

    Add project files.

---
+++ CSharpTest/WeekEnd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    /// <summary>
+    /// WeekEnd Entity
+    /// </summary>
+    public class WeekEnd
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public WeekEnd(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    /// <summary>
+    /// Calculator worker
+    /// </summary>
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            int workDays = dayCount;
+
+            try
+            {
+                if (workDays < 0)
+                {
+                    throw new Exception("dayCount can not be less than 0");
+                }
+
+                if (weekEnds != null)
+                {
+                    foreach (var weekEnd in weekEnds)
+                    {
+                        if (weekEnd.StartDate > weekEnd.EndDate)
+                        {
+                            throw new Exception("Incorrect data entered");
+                        }
+                    }
+                }
+
+                if (weekEnds == null)
+                    return startDate.AddDays(dayCount - 1);
+
+                foreach (var weekEnd in weekEnds)
+                {
+                    if (weekEnd.StartDate == weekEnd.EndDate && (startDate.AddDays(workDays + 1) < weekEnd.StartDate || startDate > weekEnd.EndDate))
+                        workDays++;
+                    if (!(startDate.AddDays(workDays - 1) < weekEnd.StartDate || startDate > weekEnd.EndDate))
+                        workDays += weekEnd.EndDate.Day - weekEnd.StartDate.Day;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                return DateTime.Now;
+            }
+
+            return startDate.AddDays(workDays);
+
+        }
+    }
+}
